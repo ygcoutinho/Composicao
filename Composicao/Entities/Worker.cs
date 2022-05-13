df71a8110f4ccf834cb98addb4bcbc329a7ab665
Yago Coutinho <yagogc.it@gmail.com>
2022-05-12 23:17:37 -0300

    Add project files.

---
+++ Composicao/Entities/Worker.cs
@@ -0,0 +1,50 @@
+﻿using Composicao.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composicao.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double  BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> contracts = new List<HourContract>();
+        public Worker() { }
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;    
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+
+            foreach(HourContract contract in contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
